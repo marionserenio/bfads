1f2febb8ef851944856846e091d413767f7c024a
marionserenio <marion.serenio@gmail.com>
2013-10-02 18:13:08 +0800

    pagination start

---
+++ articles-list.php
@@ -0,0 +1,37 @@
+<?php require('header.php') ?>
+
+	<div class="content-box shadow">
+		<div class="SearchHeader">
+			<input type="text">
+		</div>
+		<div class="SecondHeader">
+			<a class="back" href="javascript:history.back()"></a>
+			<h1>Articles and Guides</h1>
+		</div>
+		<div class="inner-content">
+			<div class="articles-list">
+				<ul>
+					<li><a href="#">2012 Appliance Black Friday Buying Guide</a></li>
+					<li><a href="#">Lorem ipsum dolor sit amet</a></li>
+					<li><a href="#">Curabitur pharetra neque id vulputate</a></li>
+					<li><a href="#">Suspendisse at urna </a></li>
+					<li><a href="#">Fusce vel eros dapibus</a></li>
+					<li><a href="#">Praesent lobortis malesuada varius</a></li>
+					<li><a href="#">Donec nunc orci, pretium nec</a></li>
+					<li><a href="#">Maecenas ac adipiscing dui</a></li>
+					<li><a href="#">Nulla vulputate dolor vitae</a></li>
+					<li><a href="#">Fusce pretium felis nec</a></li>
+					<li><a href="#">Vivamus sed blandit arcu</a></li>
+					<li><a href="#">Aenean ac pretium tellus</a></li>
+				</ul>
+			</div>
+			<div class="pagination">
+				<a class="prev"></a>
+				<a class="next"></a>
+				<ul>	
+					<li><a href="#"></a></li>
+				</ul>
+			</div>
+		</div>			
+	</div>
+<?php require('footer.php') ?>
--- hotdeals.php
+++ hotdeals.php
@@ -17,11 +17,11 @@
 								<img src="">
 							</div>
 							<div class="list-info">
-								<h3>HP Pavillion Sleekbook 15.6" Laptop w/ Intel Core i3 2377M </h3>
+								<h3>HP Pavillion Sleekbook 15.6" Laptop w/ Intel Core i3 2377M 4GB RAM, 500GB HDD, Intel HD Graphics 3000, Win 8</h3>
+								<span class="list-price">$250 <span class="list-price-old">($1,999)</span></span>
 							</div>
 						</a>
 					</li>
-					
 				</ul>
 			</div>
 		</div>			
